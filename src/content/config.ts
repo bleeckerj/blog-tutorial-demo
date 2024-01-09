2dc34237ab3631cf753dd2221991e1190584c8fe
Julian Bleecker <julian@nearfuturelaboratory.com>
2024-01-09 09:17:42 -0800

    Some updates and adjustments mostly having to do with styling and CSS issues.

---
+++ src/content/config.ts
@@ -0,0 +1,40 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const postsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string().optional(),
+      seo: z.string().optional(),
+      author: z.string(),
+      image: z.object({
+        url: z.string(),
+        alt: z.string()
+      }).optional(),
+      tags: z.array(z.string()).optional()
+    })
+});
+
+const digestCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      pubDate: z.date(),
+      cleanContent: z.string().optional(),
+      summary: z.string().optional(),
+      gpt_summary: z.string().optional(),
+      author: z.string(),
+      channel: z.string(),
+      image: z.object({
+        url: z.string(),
+        alt: z.string()
+      }).optional(),
+      tags: z.array(z.string()).optional()
+    })
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  blog: postsCollection,
+  digest: digestCollection,
+};
