2df3d8e54eb62d7dbbba8a95238ba452bed5e896
Julian Bleecker <julian@nearfuturelaboratory.com>
2024-01-20 16:06:46 -0800

    Some back office astro tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,12 +8,38 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://backoffice.nearfuturelaboratory.com",
   integrations: [preact(), tailwind(), mdx()],
   server: {
     port: 3000
   },
+  build: {
+    inlineStylesheets: `auto`,
+    assetsPrefix: 'https://backoffice.nearfuturelaboratory.com'
+  },
   renderers: [react],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'abyss-color-theme',
+      // Alternatively, provide multiple themes
+      // https://shikiji.netlify.app/guide/dual-themes#light-dark-dual-themes
+      experimentalThemes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shikiji.netlify.app/guide/transformers
+      // Find common transformers: https://shikiji.netlify.app/packages/transformers
+      transformers: [],
+    },
+  },
   // markdown: {
   //   remarkPlugins: [[remarkCustomPlugin, {/* plugin options here */}]],
   // },
